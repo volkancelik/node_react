ed0550acb31dd26ae34c6ae236004f693c2aa549
Volkan <volkan.celik@msn.com>
2019-07-10 22:28:33 +0200

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+// dev.js --- dont commit this file
+module.exports = {
+    googleClientID: '710100474078-t1nqlj63joaj4igaohq5veoum42s8clr.apps.googleusercontent.com',
+    googleClientSecret: 'rkXvC1HAE08AtZu_wJoLfM6O',
+    mongoURI: 'mongodb+srv://dbUser:syUBzTvgVJvmAD2v@emaily-fxkzw.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'safasfd'
+};
