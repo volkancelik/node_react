87e63988f3381ef608059ceb590b5fda729b3da5
Volkan <volkan.celik@msn.com>
2019-07-22 14:21:17 +0200

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
+import reducers from './reducers';
+import reduxThunk from 'redux-thunk';
+
+const store = createStore(
+    reducers
+    , {}
+    , applyMiddleware(reduxThunk));
+
+ReactDom.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.querySelector('#root')
+);
