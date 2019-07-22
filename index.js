36078637d9bdd5b2bd6314045bdec4dfa61dffc5
Volkan <volkan.celik@msn.com>
2019-07-22 19:54:17 +0200

    client side setup added

--- index.js
+++ index.js
@@ -24,5 +24,19 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.jf file
+    app.use(express.static('client/build'));
+
+    // Express will serve up index.html file
+    // if it doesnt recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
