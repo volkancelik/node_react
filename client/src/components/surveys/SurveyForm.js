34238d379233b131c2ab1f2eccc12ce63ebd51e9
Volkan <volkan.celik@msn.com>
2019-08-19 23:32:26 +0200

    chapter 161 finished

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,15 +1,23 @@
 //SurveyForm shows a form for a user to add input
 import React, { Component } from 'react';
 import { reduxForm, Field } from "redux-form";
+import { Link } from "react-router-dom";
+import _ from 'lodash';
 import SurveyField from "./SurveyField";
+
+const FIELDS = [
+    { label: 'Survey Title', name: 'title' },
+    { label: 'Subject Line', name: 'subject' },
+    { label: 'Email Body', name: 'body' },
+    { label: 'Recipient List', name: 'emails' }
+];
+
 
 class SurveyForm extends Component {
     renderFields() {
-        return (
-            <div>
-                <Field type="text" name="title" component={SurveyField}/>
-            </div>
-        );
+        return _.map(FIELDS, ({ label, name }) => {
+            return <Field key={name} component={SurveyField} type="text" label={label} name={name}/>
+        });
     }
 
     render() {
@@ -17,13 +25,31 @@ class SurveyForm extends Component {
             <div>
                 <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
                     {this.renderFields()}
-                    <button type="submit">Submit</button>
+                    <Link to="/surveys" className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button type="submit" className="teal btn-flat right white-text">
+                        Next
+                        <i className="material-icons right">done</i>
+                    </button>
                 </form>
             </div>
         )
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    _.each(FIELDS, ({ name }) => {
+        if (!values[ name ]) {
+            errors[ name ] = 'You must provide a value';
+        }
+    });
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'surveyForm'
 })(SurveyForm);
